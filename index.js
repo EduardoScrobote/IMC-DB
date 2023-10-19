606641c749a2bfcc760a3c6c85ed85ed407e0ff0
Eduardo Scrobote <EduardoScroboteProfissional@gmail.com>
2023-10-19 11:38:00 -0300

    [BUILD]: add nodemon

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const cors = require("cors");
 const server = express();
 
 server.use(cors());
+server.use(express.json());
 
 server.listen(3333, () => {
   console.log("servidor iniciado na porta 3333");
 });
 
-server.get("/userdata", (req, res) => {
-  return res.json();
+server.post("/userdata", (req, res) => {
+  const requestData = req.body;
+  console.log(requestData);
+  return res.json({ message: "dados recebidos" });
 });
